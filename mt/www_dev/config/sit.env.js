0262f3d2b64d9a297af2a4dd156a7a8d0985fae6
randyli <liruidong521@aliyun.com>
2018-04-03 19:37:09 +0800

    IOS打包

--- mt/www_dev/config/sit.env.js
+++ mt/www_dev/config/sit.env.js
@@ -2,5 +2,6 @@
 module.exports = {
   NODE_ENV: '"sit"',
   BASE_API: '"http://www.motooling.com:8080/motooling_test"'
+  // BASE_API: '"http://www.motooling.com:8080/motooling"'
   // BASE_API: '"http://192.168.2.172:8080"'
 }
